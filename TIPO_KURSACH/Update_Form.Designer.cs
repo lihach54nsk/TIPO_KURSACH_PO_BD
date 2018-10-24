bf422481cdc3ebccb05e6a91ea27fde8d0412ceb
Luzyanin Alexey <261-96@mail.ru>
2018-10-24 11:39:13 +0700

    added update form, code in progress

---
+++ TIPO_KURSACH/Update_Form.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace TIPO_KURSACH
+{
+    partial class Update_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.positionSearchTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.date_BirthSearchTextBox = new System.Windows.Forms.TextBox();
+            this.otchestvoSearchTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameSearchTextBox = new System.Windows.Forms.TextBox();
+            this.addressSearchTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lastNameSearchTextBox = new System.Windows.Forms.TextBox();
+            this.EditTextBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(63, 6);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SearchTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Поиск: ";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(12, 32);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(151, 23);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Поиск";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // positionSearchTextBox
+            // 
+            this.positionSearchTextBox.Location = new System.Drawing.Point(124, 200);
+            this.positionSearchTextBox.Name = "positionSearchTextBox";
+            this.positionSearchTextBox.Size = new System.Drawing.Size(164, 20);
+            this.positionSearchTextBox.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Должность: ";
+            // 
+            // date_BirthSearchTextBox
+            // 
+            this.date_BirthSearchTextBox.Location = new System.Drawing.Point(124, 175);
+            this.date_BirthSearchTextBox.Name = "date_BirthSearchTextBox";
+            this.date_BirthSearchTextBox.Size = new System.Drawing.Size(164, 20);
+            this.date_BirthSearchTextBox.TabIndex = 26;
+            // 
+            // otchestvoSearchTextBox
+            // 
+            this.otchestvoSearchTextBox.Location = new System.Drawing.Point(124, 123);
+            this.otchestvoSearchTextBox.Name = "otchestvoSearchTextBox";
+            this.otchestvoSearchTextBox.Size = new System.Drawing.Size(164, 20);
+            this.otchestvoSearchTextBox.TabIndex = 25;
+            // 
+            // firstNameSearchTextBox
+            // 
+            this.firstNameSearchTextBox.Location = new System.Drawing.Point(124, 97);
+            this.firstNameSearchTextBox.Name = "firstNameSearchTextBox";
+            this.firstNameSearchTextBox.Size = new System.Drawing.Size(164, 20);
+            this.firstNameSearchTextBox.TabIndex = 24;
+            // 
+            // addressSearchTextBox
+            // 
+            this.addressSearchTextBox.Location = new System.Drawing.Point(124, 149);
+            this.addressSearchTextBox.Name = "addressSearchTextBox";
+            this.addressSearchTextBox.Size = new System.Drawing.Size(164, 20);
+            this.addressSearchTextBox.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Дата рождения: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Адрес проживания: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Отчество: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Имя: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Фамилия: ";
+            // 
+            // lastNameSearchTextBox
+            // 
+            this.lastNameSearchTextBox.Location = new System.Drawing.Point(124, 69);
+            this.lastNameSearchTextBox.Name = "lastNameSearchTextBox";
+            this.lastNameSearchTextBox.Size = new System.Drawing.Size(164, 20);
+            this.lastNameSearchTextBox.TabIndex = 17;
+            // 
+            // EditTextBox
+            // 
+            this.EditTextBox.Location = new System.Drawing.Point(11, 226);
+            this.EditTextBox.Name = "EditTextBox";
+            this.EditTextBox.Size = new System.Drawing.Size(277, 23);
+            this.EditTextBox.TabIndex = 29;
+            this.EditTextBox.Text = "Изменить";
+            this.EditTextBox.UseVisualStyleBackColor = true;
+            this.EditTextBox.Click += new System.EventHandler(this.EditTextBox_Click);
+            // 
+            // Update_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 256);
+            this.Controls.Add(this.EditTextBox);
+            this.Controls.Add(this.positionSearchTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.date_BirthSearchTextBox);
+            this.Controls.Add(this.otchestvoSearchTextBox);
+            this.Controls.Add(this.firstNameSearchTextBox);
+            this.Controls.Add(this.addressSearchTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lastNameSearchTextBox);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchTextBox);
+            this.Name = "Update_Form";
+            this.Text = "Update_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.TextBox positionSearchTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox date_BirthSearchTextBox;
+        private System.Windows.Forms.TextBox otchestvoSearchTextBox;
+        private System.Windows.Forms.TextBox firstNameSearchTextBox;
+        private System.Windows.Forms.TextBox addressSearchTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox lastNameSearchTextBox;
+        private System.Windows.Forms.Button EditTextBox;
+    }
+}
---
+++ TIPO_KURSACH/Update_Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIPO_KURSACH
+{
+    public partial class Update_Form : Form
+    {
+        public Update_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditTextBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
