a3cd02dbc5333f74e8ac60e63ebd4d0cccb01579
Luzyanin Alexey <261-96@mail.ru>
2018-10-25 12:31:59 +0700

    add first update code

--- TIPO_KURSACH/Update_Form.cs
+++ TIPO_KURSACH/Update_Form.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TIPO_KURSACH
 {
     public partial class Update_Form : Form
     {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Геральт из Ривии\Documents\TIPO_KURSACH\TIPO_KURSACH\Database1.mdf;Integrated Security = True";
+
         public Update_Form()
         {
             InitializeComponent();
@@ -24,7 +27,15 @@ namespace TIPO_KURSACH
 
         private void EditTextBox_Click(object sender, EventArgs e)
         {
+            string queryString = "UPDATE dbo.Workers SET Id_position = '1', lastName = N'Я', firstName = N'ЕСТЬ', otchestvo = N'ГРУТ', address = N'ДУПЛО', date = '2222-02-02' WHERE Id_position = '1'";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
 
+            sqlConnection.Open();
+
+            SqlCommand command = new SqlCommand(queryString, sqlConnection);
+
+            command.ExecuteNonQuery();
+            sqlConnection.Close();
         }
     }
 }
