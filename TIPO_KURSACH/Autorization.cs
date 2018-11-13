eb5dafe76aaf914c011abd1a54d4b6337997336f
Luzyanin Alexey <261-96@mail.ru>
2018-11-13 14:33:35 +0700

    saved connection string

--- TIPO_KURSACH/Autorization.cs
+++ TIPO_KURSACH/Autorization.cs
@@ -8,7 +8,9 @@ namespace TIPO_KURSACH
 {
     class Autorization
     {
-        static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Геральт из Ривии\Desktop\TIPO_KURSACH\TIPO_KURSACH\Database1.mdf;Integrated Security=True";
+        static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Геральт из Ривии\Documents\TIPO_KURSACH\TIPO_KURSACH\Database1.mdf;Integrated Security=True";
+
+        //Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Геральт из Ривии\Desktop\TIPO_KURSACH\TIPO_KURSACH\Database1.mdf;Integrated Security = True
 
         public static string GetConnectionString() => connectionString;
     }    
